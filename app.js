5e2cf2a2030d852a4d73f0eb23ce0a149261dbe0
Sangita Balaji <sangita1293@gmail.com>
2025-07-10 13:37:31 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "xyz" },
@@ -5,13 +8,13 @@ const heading = React.createElement(
 );
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { key: "child" }, [
+    React.createElement("h1", {key : "1"}, "I am h1 tag"),
+    React.createElement("h2", {key : "2"}, "I am h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { key: "child2" }, [
+    React.createElement("h1", {key : "3"}, "I am h1 tag"),
+    React.createElement("h2", {key : "4"}, "I am h2 tag"),
   ]),
 ]);
 
